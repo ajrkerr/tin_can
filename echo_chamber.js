d836685da72ee37226d5c4dca5e3d96ae748f247
Adam Kerr <ajrkerr@gmail.com>
2015-11-28 11:17:50 -0500

    Sets up echo chamber to be a mini chat thing

--- echo_chamber.js
+++ echo_chamber.js
@@ -9,7 +9,8 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   console.log('a user connected');
 
-  socket.on('chat message', function (message) {
+  socket.on("chat message", function (message) {
+    socket.emit("chat message", message);
     console.log("Message Received");
     console.log(message);
   });
